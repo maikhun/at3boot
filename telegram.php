cc447c4d0ffba53d7598a00d76334006c70e6dea
Maryushkin Anton <62674062+maikhun@users.noreply.github.com>
2023-03-09 09:20:53 +0300

    Обновление формы

--- telegram.php
+++ telegram.php
@@ -2,20 +2,24 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
+$phone = $_POST['phone']
 $msg = $_POST['msg'];
+
 $token = "5648920080:AAFvRs3TPB-o8ymgnULu-Q-rSh94B6tbaqg";
 $chat_id = "-795121285";
+
 $arr = array(
-  'Имя пользователя: ' => $name,
-  'Email:' => $email,
-  'Содержание заявки:' => $msg
+  'ФИО пользователя: ' => $name,
+  'Email: ' => $email,
+  'Телефон: ' => $phone,
+  'Информация в заявке: ' => $msg
 );
 
 foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-if ($name != null && $email != null) {
+if ($name != null && $email != null && $phone != null) {
   $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 }
 
