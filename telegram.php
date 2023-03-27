d4e5aeca386c252a6809d3e0018c8b64b9dfa106
Maryushkin Anton <62674062+maikhun@users.noreply.github.com>
2023-03-27 10:06:00 +0300

    Добавление проверки
    Проверка отправляемого боту текста на наличие ссылок и проверка корректности введенного номера телефона

--- telegram.php
+++ telegram.php
@@ -2,11 +2,25 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$phone = $_POST['phone']
+$phone = $_POST['phone'];
 $msg = $_POST['msg'];
 
+if (stristr($msg, 'http') === FALSE && strlen($phone) < 12) {
+  $status = 1;
+} else {
+  $status = 0;
+}
+
+// Токен AT3BOOT_BOT
 $token = "5648920080:AAFvRs3TPB-o8ymgnULu-Q-rSh94B6tbaqg";
+
+// Рабочий чат для поступления заявок
+// $chat_id = "-913765800";
+
+// Чат для проверки работы бота
 $chat_id = "-795121285";
+
+
 
 $arr = array(
   'ФИО пользователя: ' => $name,
@@ -19,7 +33,7 @@ foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-if ($name != null && $email != null && $phone != null) {
+if ($name != null && $email != null && $phone != null && $status == 1) {
   $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 }
 
